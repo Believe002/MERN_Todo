2d25d2c7ddd1388de9832bb74f55de38adb73761
Believe002 <vicky1352002@gmail.com>
2023-05-09 13:01:16 +0530

    Changing Header

--- trainee_frontend/src/App.js
+++ trainee_frontend/src/App.js
@@ -22,7 +22,7 @@ function App() {
             <Container>
               <Navbar.Brand>
                 <Link to={'/create-student'} className="nav-link">
-                  Student Record Tracker
+                    Mern Application
                 </Link>
               </Navbar.Brand>
 
